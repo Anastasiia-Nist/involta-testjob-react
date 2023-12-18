8ddabb9eb3074c9803a69589cb93e22e3beb41b0
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-12-18 18:25:09 +0300

    feat: add HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './app/App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={'/involta-testjob-react/'}>
+    <HashRouter basename={'/involta-testjob-react/'}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
